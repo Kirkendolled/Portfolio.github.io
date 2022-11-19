6f951a1ca72db9516edcf67672fb4b29f6f1988d
Kirkendolled <112414443+Kirkendolled@users.noreply.github.com>
2022-11-19 16:41:56 -0600

    Added scroll pics

--- script.js
+++ script.js
@@ -63,3 +63,111 @@ class CssPropControl {
       function stopVideo() {
         player.stopVideo();
       }
+
+      let mybutton = document.getElementById("myBtn");
+
+//Simple button to top
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+
+
+    function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
+
+    var currentImage = 0;
+    var myPhotos=['Chadow 1.jpg', 'Leo 1.jpg', 'Sissy 1.jpg', 'Brutie.jpg','The crew.jpg'];
+    var container=document.getElementById('content');
+    var nextBtn=document.getElementById('next');
+    var prevBtn=document.getElementById('previous');
+    
+    nextBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    
+    currentImage++;
+    if (currentImage > myPhotos.length -1) {currentImage=0;}
+    
+    let newSlide = document.createElement('img');
+    newSlide.src = `Slides/${myPhotos[currentImage]}`;
+    newSlide.className = 'fadeinimg';
+    container.appendChild(newSlide);
+    if (container.children.length > 2) {
+        container.removeChild(container.children[0]);
+    }
+    });
+    prevBtn.addEventListener('click', function(event){
+        event.preventDefault();
+        currentImage--;
+        if (currentImage > myPhotos.length -1) {currentImage=0;}
+    
+    let newSlides = document.createElement('img');
+    newSlides.src = `Slides/${myPhotos[currentImage]}`;
+    newSlides.className = 'fadeinimg';
+    container.appendChild(newSlides);
+    if (container.children.length > 2) {
+        container.removeChild(container.children[0]);
+    }    
+    }); 
+
+    const videosContainer = document.getElementById('videosContainer');
+    const popup = document.getElementById('popup');
+    const video = document.querySelector('#popup > iframe');
+    const videoIdInput = document.getElementById('videoId');
+    const IDS_KEY = 'youTubeVideoIds';
+    let youTubeVideoIds = [];
+
+    const loadVideos = () => {
+      youTubeVideoIds = JSON.parse(localStorage.getItem(IDS_KEY)) || [];
+    };
+
+    const displayVideos = () => {
+      const videosStr = youTubeVideoIds
+        .map(
+          (id) => `
+          <li onclick="clickVideo(event, '${id}')">
+            <img class="thumbnail" src="https://i3.ytimg.com/vi/${id}/sddefault.jpg" alt="Cover image for YouTube video with id ${id}">
+            <button class="delete-btn" >&times;</button>
+          </li>
+        `
+        )
+        .join('');
+      videosContainer.innerHTML = videosStr;
+    };
+
+    const clickVideo = (event, id) => {
+      if (event?.target?.classList?.contains('delete-btn')) {
+        youTubeVideoIds = youTubeVideoIds.filter((i) => i !== id);
+        localStorage.setItem(IDS_KEY, JSON.stringify(youTubeVideoIds));
+        displayVideos();
+      } else {
+        video.src = `https://www.youtube.com/embed/${id}`;
+        popup.classList.add('open');
+        popup.classList.remove('closed');
+      }
+    };
+
+    const handlePopupClick = () => {
+      popup.classList.add('closed');
+      popup.classList.remove('open');
+    };
+
+    const saveVideo = (e) => {
+      e.preventDefault();
+      const videoId = videoIdInput.value;
+      videoIdInput.value = '';
+      youTubeVideoIds.unshift(videoId);
+      localStorage.setItem(IDS_KEY, JSON.stringify(youTubeVideoIds));
+      displayVideos();
+    };
+
+    loadVideos();
+    displayVideos();
+        
